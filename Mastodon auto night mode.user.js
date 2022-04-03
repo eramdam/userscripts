93c4a1c426bd2c86e1e42296819e4e1b5e6e92e3
Damien Erambert <damien@erambert.me>
2022-04-03 13:22:47 +0200

    Update Mastodon auto night mode.user.js

--- Mastodon auto night mode.user.js
+++ Mastodon auto night mode.user.js
@@ -5,41 +5,39 @@
 // @version     1.0.0
 // @author      @Eramdam
 // @namespace   eramdam
+// @run-at      document-start
 // ==/UserScript==
 
-// `copy([...document.querySelectorAll('link[rel=stylesheet]')].map(i => i.href))
+// `copy([document.querySelector('link[rel=stylesheet][href*="chunk."]')?.href, document.querySelector('link[rel=stylesheet][href*="chunk."]')?.integrity)
 // These URLs are gonna break next time the instance gets deployed...
 const whiteTheme = [
-  [
-    'https://octodon.social/packs/css/mastodon-light-e831b6ee.chunk.css',
-    'sha256-KcsfarZX47nuIGhRW29a6D4sXiYIJtezpTyJGHgSqM8=',
-  ],
+  'https://octodon.social/packs/css/mastodon-light-e831b6ee.chunk.css',
+  'sha256-KcsfarZX47nuIGhRW29a6D4sXiYIJtezpTyJGHgSqM8=',
 ];
 
 const darkTheme = [
-  [
-    'https://octodon.social/packs/css/default-4fb6b0ab.chunk.css',
-    'sha256-d0k6MMFKhwfoTs1uMLgY1D5uS9KuxxUtLdthw7FScgo=',
-  ],
+  'https://octodon.social/packs/css/default-4fb6b0ab.chunk.css',
+  'sha256-d0k6MMFKhwfoTs1uMLgY1D5uS9KuxxUtLdthw7FScgo=',
 ];
 
 const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
 const onSystemDarkModeChange = (ev) => {
   const hasSystemDarkMode = ev.matches;
-  const themePairs = hasSystemDarkMode ? darkTheme : whiteTheme;
+  const [theme, integrity] = hasSystemDarkMode ? darkTheme : whiteTheme;
+  const styleToChange = document.querySelector(
+    'link[rel=stylesheet][href*="chunk."]'
+  );
 
-  themePairs.forEach((pair) => {
-    const [theme, integrity] = pair;
+  if (styleToChange) {
     const newThemeLink = document.createElement('link');
     newThemeLink.rel = 'stylesheet';
     newThemeLink.href = theme;
     newThemeLink.integrity = integrity;
-
     document.head.appendChild(newThemeLink);
-  });
+    styleToChange.remove();
+  }
 };
 
 mediaQuery.addEventListener('change', onSystemDarkModeChange);
-
 onSystemDarkModeChange({ matches: mediaQuery.matches });
