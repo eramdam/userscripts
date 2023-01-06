90925dd5b6f6e4794add6d2b4d48635ba56e9bc9
Damien Erambert <damien@erambert.me>
2023-01-06 11:12:41 -0800

    Update Mastodon auto night mode.user.js

--- Mastodon auto night mode.user.js
+++ Mastodon auto night mode.user.js
@@ -1,3 +1,4 @@
+// @ts-check
 // ==UserScript==
 // @name        Mastodon auto night mode
 // @description Switch between dark and light theme on Mastodon
@@ -12,46 +13,64 @@
 // These URLs are gonna break next time the instance gets deployed...
 const vanillaLightThemeStyles = [
   [
-    'https://octodon.social/packs/css/core/common-50d0784d.chunk.css',
-    'sha256-c4lMl/Kd1OgQQfEufaXmD+M2j07FJEKma1Mu6eFoP/g=',
+    'https://octodon.social/packs/css/core/common-a844dc34.chunk.css',
+    'sha256-TqG4aY9sRserz6ezqW5i6LhnoG/zZTkQ9DdiaCAc13o=',
   ],
   [
-    'https://octodon.social/packs/css/skins/vanilla/mastodon-light/common-d3c87180.chunk.css',
-    'sha256-+nJK4FuNpVwKYQkJKpEUWd7Db27alINWb9DoswX6qxs=',
+    'https://octodon.social/packs/css/skins/vanilla/mastodon-light/common-34bcf7b4.chunk.css',
+    'sha256-1+qK9rpxf4CSE0zvqtL9UhvZzRMHkqHlU7sbw0sumwo=',
   ],
 ];
 
 const glitchLightThemeStyles = [
   [
-    'https://octodon.social/packs/css/core/common-50d0784d.chunk.css',
-    'sha256-c4lMl/Kd1OgQQfEufaXmD+M2j07FJEKma1Mu6eFoP/g=',
+    'https://octodon.social/packs/css/core/common-fccd12a2.chunk.css',
+    'sha256-QXkqWo4J20SxB8f1zQm39dnjZ4bRvrDmOHL6VMSgrOQ=',
   ],
   [
-    'https://octodon.social/packs/css/skins/glitch/mastodon-light/common-774b8e36.chunk.css',
-    'sha256-FUaNcgbHDxthAiEw5Spcwwi04nhnOMmOIxxQWhqA7yM=',
+    'https://octodon.social/packs/css/skins/glitch/mastodon-light/common-e57f4dec.chunk.css',
+    'sha256-5/eZZaLBtX3L/vAliFDJcbwA8BWz5PS4BTtdOBKQuZc=',
+  ],
+];
+
+const glitchDarkThemeStyles = [
+  [
+    'https://octodon.social/packs/css/core/common-fccd12a2.chunk.css',
+    'sha256-QXkqWo4J20SxB8f1zQm39dnjZ4bRvrDmOHL6VMSgrOQ=',
+  ],
+  [
+    'https://octodon.social/packs/css/flavours/glitch/common-1c295280.chunk.css',
+    'sha256-yPa7+jD8s8XCYP2MT06UaGLR8/o5qdCkf1lxwzUNb+I=',
   ],
 ];
 
 const vanillaDarkThemeStyles = [
   [
-    'https://octodon.social/packs/css/core/common-50d0784d.chunk.css',
-    'sha256-c4lMl/Kd1OgQQfEufaXmD+M2j07FJEKma1Mu6eFoP/g=',
+    'https://octodon.social/packs/css/core/common-a844dc34.chunk.css',
+    'sha256-TqG4aY9sRserz6ezqW5i6LhnoG/zZTkQ9DdiaCAc13o=',
   ],
   [
-    'https://octodon.social/packs/css/flavours/vanilla/common-ee9ef219.chunk.css',
-    'sha256-HYSGgi4ge38qrjvY1WOscY3yyuuAGvZtQcxeE7QSbiI=',
+    'https://octodon.social/packs/css/flavours/vanilla/common-20dcab1a.chunk.css',
+    'sha256-I3Fzks5AWaz4NKRSotULDINUCtrqnG0CKiynlH1aC/c=',
   ],
 ];
-const glitchDarkThemeStyles = [
-  [
-    'https://octodon.social/packs/css/core/common-50d0784d.chunk.css',
-    'sha256-c4lMl/Kd1OgQQfEufaXmD+M2j07FJEKma1Mu6eFoP/g=',
-  ],
-  [
-    'https://octodon.social/packs/css/flavours/glitch/common-97cc3205.chunk.css',
-    'sha256-nabwQ9NgKl/NuzQyfeMuyhQVfIVv1eg0Hu8AkyZtMGs=',
-  ],
-];
+
+vanillaDarkThemeStyles.forEach((newStyle) => {
+  const newStyleLink = document.createElement('link');
+  newStyleLink.rel = 'stylesheet';
+  newStyleLink.href = newStyle[0];
+  newStyleLink.integrity = newStyle[1];
+  newStyleLink.media = '(prefers-color-scheme: dark)';
+  document.documentElement.appendChild(newStyleLink);
+});
+vanillaLightThemeStyles.forEach((newStyle) => {
+  const newStyleLink = document.createElement('link');
+  newStyleLink.rel = 'stylesheet';
+  newStyleLink.href = newStyle[0];
+  newStyleLink.integrity = newStyle[1];
+  newStyleLink.media = '(prefers-color-scheme: light)';
+  document.documentElement.appendChild(newStyleLink);
+});
 
 new MutationObserver((_mut, observer) => {
   const originalStyles = document.querySelectorAll(
@@ -59,52 +78,23 @@ new MutationObserver((_mut, observer) => {
   );
 
   if (originalStyles.length > 1) {
+    observer.disconnect();
     originalStyles.forEach((e) => e.remove());
+
+    setTimeout(() => {
+      if (navigator.platform.includes('Mac')) {
+        [...document.styleSheets].forEach((sheet) => {
+          Object.entries(sheet.cssRules).forEach(([index, rule]) => {
+            if (rule instanceof CSSStyleRule) {
+              if (rule.selectorText.includes('::-webkit-scrollbar')) {
+                sheet.deleteRule(Number(index));
+              }
+            }
+          });
+        });
+      }
+    }, 1);
   }
-
-  const isReady = document.querySelector('body.app-body,body.admin');
-
-  if (!isReady) {
-    return;
-  }
-
-  if (document.querySelector('body.flavour-vanilla')) {
-    vanillaDarkThemeStyles.forEach((newStyle) => {
-      const newStyleLink = document.createElement('link');
-      newStyleLink.rel = 'stylesheet';
-      newStyleLink.href = newStyle[0];
-      newStyleLink.integrity = newStyle[1];
-      newStyleLink.media = '(prefers-color-scheme: dark)';
-      document.documentElement.appendChild(newStyleLink);
-    });
-    vanillaLightThemeStyles.forEach((newStyle) => {
-      const newStyleLink = document.createElement('link');
-      newStyleLink.rel = 'stylesheet';
-      newStyleLink.href = newStyle[0];
-      newStyleLink.integrity = newStyle[1];
-      newStyleLink.media = '(prefers-color-scheme: light)';
-      document.documentElement.appendChild(newStyleLink);
-    });
-  } else {
-    glitchDarkThemeStyles.forEach((newStyle) => {
-      const newStyleLink = document.createElement('link');
-      newStyleLink.rel = 'stylesheet';
-      newStyleLink.href = newStyle[0];
-      newStyleLink.integrity = newStyle[1];
-      newStyleLink.media = '(prefers-color-scheme: dark)';
-      document.documentElement.appendChild(newStyleLink);
-    });
-    glitchLightThemeStyles.forEach((newStyle) => {
-      const newStyleLink = document.createElement('link');
-      newStyleLink.rel = 'stylesheet';
-      newStyleLink.href = newStyle[0];
-      newStyleLink.integrity = newStyle[1];
-      newStyleLink.media = '(prefers-color-scheme: light)';
-      document.documentElement.appendChild(newStyleLink);
-    });
-  }
-
-  observer.disconnect();
 }).observe(document.documentElement, {
   subtree: true,
   childList: true,
--- README.md
+++ README.md
@@ -10,14 +10,15 @@ As the time of writing (February 2021), I use those scripts with [Userscripts](h
 
 <!-- Insert files table -->
 
-| Name                                                                                                                          | Author   | Description                                         |
-| ----------------------------------------------------------------------------------------------------------------------------- | -------- | --------------------------------------------------- |
-| [Auto-dark mode - fastmail.com](https://github.com/eramdam/userscripts/blob/main/Auto-dark%20mode%20-%20fastmail.com.user.js) | @Eramdam | 6/15/2021, 9:47:45 PM                               |
-| [Club iGen disable share api](https://github.com/eramdam/userscripts/blob/main/Club%20iGen%20disable%20share%20api.user.js)   | @Eramdam | Disables the Share API on clubigen.fr               |
-| [GDQ schedule dimmer](https://github.com/eramdam/userscripts/blob/main/GDQ%20schedule%20dimmer.user.js)                       | @eevee   | https://twitter.com/eevee/status/950119009638232064 |
-| [Mastodon auto night mode](https://github.com/eramdam/userscripts/blob/main/Mastodon%20auto%20night%20mode.user.js)           | @Eramdam | Switch between dark and light theme on Mastodon     |
-| [Minimal Tumblr](https://github.com/eramdam/userscripts/blob/main/Minimal%20Tumblr.css)                                       | @Eramdam | Simpler Tumblr style                                |
-| [New Reddit auto night mode](https://github.com/eramdam/userscripts/blob/main/New%20Reddit%20auto%20night%20mode.user.js)     | @Eramdam | Switches night mode on/off in Reddit's new layout   |
-| [Old Reddit auto night mode](https://github.com/eramdam/userscripts/blob/main/Old%20Reddit%20auto%20night%20mode.user.js)     | @Eramdam | Switches night mode on/off in Reddit's old layout   |
-| [Screenshot Plex](https://github.com/eramdam/userscripts/blob/main/Screenshot%20Plex.user.js)                                 | @Eramdam | Add a screenshot button to Plex's player            |
-| [Screenshot Twitch](https://github.com/eramdam/userscripts/blob/main/Screenshot%20Twitch.user.js)                             | @Eramdam | Adds a screenshot button to Twitch's player         |
+| Name                                                                                                                          | Author   | Description                                                          |
+| ----------------------------------------------------------------------------------------------------------------------------- | -------- | -------------------------------------------------------------------- |
+| [Auto-dark mode - fastmail.com](https://github.com/eramdam/userscripts/blob/main/Auto-dark%20mode%20-%20fastmail.com.user.js) | @Eramdam | 6/15/2021, 9:47:45 PM                                                |
+| [Club iGen disable share api](https://github.com/eramdam/userscripts/blob/main/Club%20iGen%20disable%20share%20api.user.js)   | @Eramdam | Disables the Share API on clubigen.fr                                |
+| [GDQ schedule dimmer](https://github.com/eramdam/userscripts/blob/main/GDQ%20schedule%20dimmer.user.js)                       | @eevee   | https://twitter.com/eevee/status/950119009638232064                  |
+| [Compact(er) Mastodon](https://github.com/eramdam/userscripts/blob/main/mastocompact.css)                                     | Eramdam  | Tweaks the Mastodon multi-column UI to make it slightly more compact |
+| [Mastodon auto night mode](https://github.com/eramdam/userscripts/blob/main/Mastodon%20auto%20night%20mode.user.js)           | @Eramdam | Switch between dark and light theme on Mastodon                      |
+| [Minimal Tumblr](https://github.com/eramdam/userscripts/blob/main/Minimal%20Tumblr.css)                                       | @Eramdam | Simpler Tumblr style                                                 |
+| [New Reddit auto night mode](https://github.com/eramdam/userscripts/blob/main/New%20Reddit%20auto%20night%20mode.user.js)     | @Eramdam | Switches night mode on/off in Reddit's new layout                    |
+| [Old Reddit auto night mode](https://github.com/eramdam/userscripts/blob/main/Old%20Reddit%20auto%20night%20mode.user.js)     | @Eramdam | Switches night mode on/off in Reddit's old layout                    |
+| [Screenshot Plex](https://github.com/eramdam/userscripts/blob/main/Screenshot%20Plex.user.js)                                 | @Eramdam | Add a screenshot button to Plex's player                             |
+| [Screenshot Twitch](https://github.com/eramdam/userscripts/blob/main/Screenshot%20Twitch.user.js)                             | @Eramdam | Adds a screenshot button to Twitch's player                          |
