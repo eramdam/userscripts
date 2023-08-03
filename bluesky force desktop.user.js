ce1916d789fbdcb16bb0d7d9bf1323fb4f4d181c
Damien Erambert <damien@erambert.me>
2023-08-02 23:25:28 -0700

    Create bluesky force desktop.user.js

---
+++ bluesky force desktop.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Force Bsky.app to desktop mode
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Force Bsky.app to desktop mode
+// @author       Eramdam
+// @match        https://bsky.app/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bsky.app
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const baseMatchMedia = window.matchMedia;
+  // Monkey patch matchMedia to make the React code think we're always in desktop mode
+  window.matchMedia = (query) => {
+    if (query === 'only screen and (max-width: 1230px)') {
+      return {
+        matches: false,
+        addListener: () => {},
+      };
+    }
+
+    if (query === '(min-width: 1230px)') {
+      return {
+        matches: true,
+        addListener: () => {},
+      };
+    }
+
+    return baseMatchMedia(query);
+  };
+
+  // Hide the right sidebar
+  const style = document.createElement('style');
+  style.innerText =
+    'html body div#root div.css-175oi2r.r-13awgt0 div.css-175oi2r.r-1pi2tsx.r-kemksi div.css-175oi2r.r-1u20jyi.r-u8s1d.r-1ej1qmr.r-ryqm5k {display: none;}';
+  document.body.appendChild(style);
+})();
