0098015389fc922ab9c39422e974756e2abc776c
Damien Erambert <damien@erambert.me>
2024-02-04 10:44:27 -0800

    Update infinite-craft-save.user.js

--- infinite-craft-save.user.js
+++ infinite-craft-save.user.js
@@ -3,7 +3,7 @@
 // @name        Export/Import infinite craft data
 // @grant       none
 // @match       https://neal.fun/infinite-craft/
-// @version     1.0
+// @version     1.1
 // ==/UserScript==
 
 (async () => {
@@ -17,13 +17,14 @@
     reader = new FileReader();
     reader.onload = (e) => {
       localStorage.setItem('infinite-craft-data', e.target.result);
+      window.location.reload();
     };
     console.log(fileInput.files[0]);
     reader.readAsText(fileInput.files[0]);
   };
   document.body.appendChild(fileInput);
 
-  fileSelect = document.createElement('button');
+  const fileSelect = document.createElement('button');
   fileSelect.onclick = () => {
     fileInput.click();
   };
@@ -32,7 +33,7 @@
 
   document.body.appendChild(fileSelect);
 
-  fileDownload = document.createElement('button');
+  const fileDownload = document.createElement('button');
   fileDownload.style = 'position: fixed; bottom: 0; left: 35vw;';
   fileDownload.innerText = 'Export';
   fileDownload.onclick = () => {
